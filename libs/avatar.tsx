186c164b01cee013b3d89aace9ac223914382f8c
Piamias <77303804+Piamias@users.noreply.github.com>
2022-03-19 14:35:20 +0100

    avatars

---
+++ libs/avatar.tsx
@@ -0,0 +1,8 @@
+export const avatars = {
+  "hgicquel": "https://ipfs.io/ipfs/QmRRPWG96cmgTn2qSzjwr2qvfNEuhunv6FNeMFGa9bx6mQ",
+  "vpiamias": "https://ipfs.io/ipfs/QmYxT4LnK8sqLupjbS6eRvu1si7Ly2wFQAqFebxhWntcf6",
+  "brmasser": "https://ipfs.io/ipfs/QmSR1aCRtn6cGrJaWNog586943N8BgfrXLKzAavCfjSuzE",
+  "abfall": "https://ipfs.io/ipfs/QmR9uR6ufPnTrz2eFVrhgESEaU47Y7F4UE5pYorHNYR9XF",
+  "cvidon": "https://ipfs.io/ipfs/QmRmb5QYpWN1dF32UcVTPMsVwHubiiM7wC9SpacXpg1XHc",
+  "lkhamlac": "https://ipfs.io/ipfs/QmRsJLrg27GQ1ZWyrXZFuJFdU5bapfzsyBfm3CAX1V1bw6"
+}
