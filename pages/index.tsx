3084549196ea60d3158564911421e1cb53561a69
Piamias <77303804+Piamias@users.noreply.github.com>
2022-03-19 14:36:17 +0100

    row

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,95 +1,41 @@
+import { use42 } from "libs/fetch";
+import Link from "next/link";
+import { Fragment } from "react";
 
+const logins = [
+	"hgicquel",
+	"brmasser",
+	"abfall",
+	"cvidon",
+	"lkhamlac",
+	"vpiamias"
+]
+
+interface Profile {
+	id: number,
+	login: string
+}
+
+function _Profile(props: {
+	profile: Profile
+}) {
+	const { profile } = props
+
+	return <Link href={`/user/${profile.login}`} passHref>
+		<a className="p-2 flex border border-contrast rounded-xl mb-2">
+			{/* <img src={} /> */}
+			{profile.login}
+		</a>
+	</Link>
+}
 
 export default function Home() {
+	const	users = use42<Profile[]>(`/v2/users?filter[login]=${logins.join(',')}`)
+
 	return (<>
-		<div className=" rounded-lg inline-block w-full mr-6 ">
-					<span className="font-semibold text-xl tracking-tight ">Ranking</span>
-					<table className="titre">
-						<thead>
-							<tr>
-								<th scope="col"> Position</th>
-								<th scope="col"> Pic </th>
-								<th scope="col"> Name </th>
-								<th scope="col"> Level </th>
-								<th scope="col"> Link </th>
-								<th scope="col"> Country </th>
-							</tr>
-						</thead>
-						<tbody className="row">
-							<tr id="cvidon">
-								<td>1</td>
-								<td>
-									{/* <Image src={icon} width="50" height="50" /> */}
-								</td>
-								<td> cvidon </td>
-								<td> 9.7% </td>
-								<td>
-									{" "}
-									<a href="https://profile.intra.42.fr/users/cvidon">
-										{" "}
-										Link{" "}
-									</a>{" "}
-								</td>
-								<td>
-									{/* <Image src={flag} width="50" height="35" /> */}
-								</td>
-							</tr>
-							<tr id="cvidon">
-								<td>1</td>
-								<td>
-									{/* <Image src={icon} width="50" height="50" /> */}
-								</td>
-								<td> cvidon </td>
-								<td> 9.7% </td>
-								<td>
-									{" "}
-									<a href="https://profile.intra.42.fr/users/cvidon">
-										{" "}
-										Link{" "}
-									</a>{" "}
-								</td>
-								<td>
-									{/* <Image src={flag} width="50" height="35" /> */}
-								</td>
-							</tr>
-							<tr id="cvidon">
-								<td>1</td>
-								<td>
-									{/* <Image src={icon} width="50" height="50" /> */}
-								</td>
-								<td> cvidon </td>
-								<td> 9.7% </td>
-								<td>
-									{" "}
-									<a href="https://profile.intra.42.fr/users/cvidon">
-										{" "}
-										Link{" "}
-									</a>{" "}
-								</td>
-								<td>
-									{/* <Image src={flag} width="50" height="35" /> */}
-								</td>
-							</tr>
-							<tr id="brmasser">
-								<td>2</td>
-								<td>
-									{/* <Image src={icon} width="50" height="50" /> */}
-								</td>
-								<td> brmasser </td>
-								<td> 8.7% </td>
-								<td>
-									{" "}
-									<a href="https://profile.intra.42.fr/users/brmasser">
-										{" "}
-										Link{" "}
-									</a>{" "}
-								</td>
-								<td>
-									{/* <Image src={flag} width="50" height="35" /> */}
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
+		{users?.map(user =>
+			<Fragment key={user.id}>
+				<_Profile profile={user} />
+			</Fragment>)}
 	</>)
 }
