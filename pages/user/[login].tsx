87d1a6e9c73868cd6a68db8bc640fc08f13ffc3a
Piamias <77303804+Piamias@users.noreply.github.com>
2022-03-19 15:28:21 +0100

    profile page

--- pages/user/[login].tsx
+++ pages/user/[login].tsx
@@ -1,13 +1,45 @@
-import { useRouter } from "next/router"
+import { avatars } from "libs/avatars";
+import { use42 } from "libs/fetch";
+import { useRouter } from "next/router";
+
+interface Profile {
+	id: number,
+	login: string,
+	first_name: string,
+	last_name: string
+}
+
+function _Profile(props: {
+	profile: Profile
+}) {
+	const { profile } = props
+
+	return <a className="flex border border-contrast rounded-xl mb-2">
+		<div className="p-4 flex">
+			<img className="w-12 h-12 rounded-full mr-4"
+				src={avatars[profile.login]} />
+			<div>
+				<div>{profile.login}</div>
+				<div className="text-contrast">
+					{profile.first_name} {profile.last_name}
+				</div>
+			</div>
+		</div>
+	</a>
+}
 
 function _Page(props: {
 	login: string
 }) {
 	const { login } = props
 
-	return (<>
-		Page de profil de {login}
-	</>)
+	const	users = use42<Profile[]>(`/v2/users?filter[login]=${login}`)
+	const profile = users?.[0]
+
+	if (!profile)
+		return <>Loading...</>
+
+	return <_Profile profile={profile} />
 }
 
 export default function Profile() {
