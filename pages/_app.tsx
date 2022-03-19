b863ce44c77599c1128ef34809525a3190f5a72d
blessedNholyflavored <67589711+blessedNholyflavored@users.noreply.github.com>
2022-03-19 12:51:55 +0100

    lucie

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 import { ChangeEvent, useCallback, useState } from 'react'
+import logo from "../public/assets/fusee.png"
 import '../styles/globals.css'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [search, setSearch] = useState<string>()
@@ -16,6 +19,38 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<h1 className="text-8xl m-auto font-bold">
 					Cursus3.io
 				</h1>
+
+				<nav className="flex items-center justify-between flex-wrap bg-blue-800 p-6">
+					<div className="flex items-center flex-shrink-0 text-white mr-6">
+						<Image src={logo} width="100" height="50" />
+
+						<span className="font-semibold text-xl tracking-tight">
+							Chad3.io
+						</span>
+					</div>
+					<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+						<div className="text-sm lg:flex-grow">
+							<a
+								href="#responsive-header"
+								className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+								Test1
+							</a>
+							<a
+								href="#responsive-header"
+								className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+								Test2
+							</a>
+						</div>
+						<div>
+							<a
+								href="#"
+								className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+								Espace Candidat
+							</a>
+						</div>
+					</div>
+				</nav>
+
 				<div className="my-2" />
 				<div className="w-full">
 					<input className="w-full bg-contrast rounded-xl p-4"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,98 @@
+import Image from "next/image";
+import icon from "../public/assets/boredape.png";
+import flag from "../public/assets/flag.png";
+
 
 export default function Home() {
 	return (<>
-		Classement
+		<div className=" rounded-lg inline-block w-full mr-6 ">
+					<span className="font-semibold text-xl tracking-tight ">Ranking</span>
+					<table className="titre">
+						<thead>
+							<tr>
+								<th scope="col"> Position</th>
+								<th scope="col"> Pic </th>
+								<th scope="col"> Name </th>
+								<th scope="col"> Level </th>
+								<th scope="col"> Link </th>
+								<th scope="col"> Country </th>
+							</tr>
+						</thead>
+						<tbody className="row">
+							<tr id="cvidon">
+								<td>1</td>
+								<td>
+									<Image src={icon} width="50" height="50" />
+								</td>
+								<td> cvidon </td>
+								<td> 9.7% </td>
+								<td>
+									{" "}
+									<a href="https://profile.intra.42.fr/users/cvidon">
+										{" "}
+										Link{" "}
+									</a>{" "}
+								</td>
+								<td>
+									<Image src={flag} width="50" height="35" />
+								</td>
+							</tr>
+							<tr id="cvidon">
+								<td>1</td>
+								<td>
+									<Image src={icon} width="50" height="50" />
+								</td>
+								<td> cvidon </td>
+								<td> 9.7% </td>
+								<td>
+									{" "}
+									<a href="https://profile.intra.42.fr/users/cvidon">
+										{" "}
+										Link{" "}
+									</a>{" "}
+								</td>
+								<td>
+									<Image src={flag} width="50" height="35" />
+								</td>
+							</tr>
+							<tr id="cvidon">
+								<td>1</td>
+								<td>
+									<Image src={icon} width="50" height="50" />
+								</td>
+								<td> cvidon </td>
+								<td> 9.7% </td>
+								<td>
+									{" "}
+									<a href="https://profile.intra.42.fr/users/cvidon">
+										{" "}
+										Link{" "}
+									</a>{" "}
+								</td>
+								<td>
+									<Image src={flag} width="50" height="35" />
+								</td>
+							</tr>
+							<tr id="brmasser">
+								<td>2</td>
+								<td>
+									<Image src={icon} width="50" height="50" />
+								</td>
+								<td> brmasser </td>
+								<td> 8.7% </td>
+								<td>
+									{" "}
+									<a href="https://profile.intra.42.fr/users/brmasser">
+										{" "}
+										Link{" "}
+									</a>{" "}
+								</td>
+								<td>
+									<Image src={flag} width="50" height="35" />
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 	</>)
 }
