301b774989402fc93e5feaeae18897d9e19d225a
Piamias <77303804+Piamias@users.noreply.github.com>
2022-03-20 12:57:05 +0100

    image ++

--- pages/epitech.tsx
+++ pages/epitech.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return <div className="rounded-xl flex flex-col items-center justify-center ">
 	<img className="h-32 w-32"
-		src="https://o.remove.bg/downloads/04847e16-3235-4308-805a-323dd7b74e34/Sans_titre__2_-removebg-preview.png"/>
+		src="https://ipfs.io/ipfs/QmVBtyoiqy7VLEZcYP8fbC4XbPY5fTs6U5RkxWKUtm7vQu"/>
 	<a className="text-3xl font-bold font-sans mb-8">Epitech is coming soon...</a>
 	</div>
 }
