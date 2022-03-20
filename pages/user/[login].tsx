46cdcc000129807a75b7adf214f4aae12eb4d098
Piamias <77303804+Piamias@users.noreply.github.com>
2022-03-20 12:58:42 +0100

    css

--- pages/user/[login].tsx
+++ pages/user/[login].tsx
@@ -82,8 +82,8 @@ function _Project(props: {
 	if (!project)
 		return null
 	return <>
-		<div className="relative p-4 border-l border-contrast">
-			<span className="absolute -translate-y-6 -translate-x-8 w-6 h-6 flex justify-center items-center bg-blue-100 rounded-full ring-8 ring-white">
+		<div className="relative ml-2 p-4 border-l border-contrast">
+			<span className="absolute -translate-y-6 -translate-x-7 w-6 h-6 flex justify-center items-center bg-blue-100 rounded-full">
 				<svg className="w-3 h-3 text-blue-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 					<path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path>
 				</svg>
